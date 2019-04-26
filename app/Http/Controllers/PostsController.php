5bf42c71b27e5383a9f11e43a153c01cb93e8a51
kamanzidanny <shikirid@gmail.com>
2019-04-26 05:12:49 +0200

    user authontication

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -21,7 +21,7 @@ class PostsController extends Controller
         /* displaying the posts by descending order with consideration of title */
         //$posts = Post::orderBy('title','desc')->get();
 
-        $posts = Post::orderBy('title','desc')->paginate(1);
+        $posts = Post::orderBy('created_at','desc')->paginate(10);
         return view('posts.index')->with('posts',$posts);
     }
 
@@ -32,7 +32,7 @@ class PostsController extends Controller
      */
     public function create()
     {
-        //
+        return view('posts.create');
     }
 
     /**
@@ -43,7 +43,19 @@ class PostsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+        
+        //create Post
+        $post = new Post;
+        $post->title = $request->input('title');
+        $post->body = $request->input('body');
+        $post->user_id = auth()->user()->id;
+        $post->save();
+
+        return redirect('/posts')->with('success','Post Created');
     }
 
     /**
@@ -66,7 +78,8 @@ class PostsController extends Controller
      */
     public function edit($id)
     {
-        //
+        $post = Post::find($id);
+        return view('posts.edit')->with('post',$post);
     }
 
     /**
@@ -78,7 +91,18 @@ class PostsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request, [
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+        
+        //Update Post
+        $post = Post::find($id);
+        $post->title = $request->input('title');
+        $post->body = $request->input('body');
+        $post->save();
+
+        return redirect('/posts')->with('success','Post Updated');
     }
 
     /**
@@ -89,6 +113,9 @@ class PostsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+        $post->delete();
+
+        return redirect('/posts')->with('success','Post removed');
     }
 }
--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,14 +1,51 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="/">LSAPP</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-item nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="/about">About</a>
-      <a class="nav-item nav-link" href="/services">Services</a>
-      <a class="nav-item nav-link" href="/posts">Blog</a>
+<nav class="navbar navbar-inverse">
+  <div class="container">
+    <div class="navbar-header">
+      <!-- Collapsed Hamburger -->
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+        <span class="sr-only">Toggle Navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+
+      <!-- Branding Image -->
+      <a class="navbar-brand" href="/">LSAPP</a>
     </div>
+    <div class="collapse navbar-collapse" id="app-navbar-collapse">
+        <!-- Left Side Of Navbar -->
+        <ul class="nav navbar-nav">
+          <li><a class="nav-item nav-link" href="/">Home <span class="sr-only">(current)</span></a></li>
+          <li><a class="nav-item nav-link" href="/about">About</a></li>
+          <li><a class="nav-item nav-link" href="/services">Services</a></li>
+          <li><a class="nav-item nav-link" href="/posts">Blog</a></li>
+          <li><a class="nav-item nav-link" href="/posts/create">Create Post</a></li>
+        </ul>       
+        <!-- Right Side Of Navbar -->
+        <ul class="nav navbar-nav navbar-right">
+            <!-- Authentication Links -->
+            @if (Auth::guest())
+              <li><a href="{{ route('login') }}">Login</a></li>
+              <li><a href="{{ route('register') }}">Register</a></li>
+            @else
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                  {{ Auth::user()->name }} <span class="caret"></span>
+                </a>
+                <ul class="dropdown-menu" role="menu">
+                  <li><a href="/dashboard">Dashboard</a></li>
+                  <li>
+                    <a href="{{ route('logout') }}"onclick="event.preventDefault();document.getElementById('logout-form').submit();">Logout
+                    </a>
+                    
+                    <form id="logout-form" action="{{ route('logout') }}"  method="POST" style="display: none;">
+                                      {{ csrf_field() }}
+                    </form>
+                    </li>
+                </ul>
+              </li>
+            @endif
+          </ul>
+        </div>
   </div>
 </nav>
